e51029038018796aecafd3a4835df6c58ef68d61
juglika <lashajugeli96@gmail.com>
2025-03-28 20:38:21 +0400

    ლექცია 7

--- test.py
+++ test.py
@@ -1,3 +1,22 @@
-import my_module
+# import my_module
 
-print(my_module.COOL_NAME)
+# print(my_module.COOL_NAME)
+
+n = int(input("ჩაწერეთ ნატურალური რიცხვი:"))
+
+i = 1
+
+m = abs(n)
+
+
+while i <  m:
+    if m == i * i * i:
+        if m ==n:
+            print(f"კუბური ფესვი არის {i}")
+        else:
+            print(f"კუბური ფესვი არის {i * -1}")
+        break
+    else:
+        i +=1
+if i == m:
+    print('ვერ ვიპოვე')
